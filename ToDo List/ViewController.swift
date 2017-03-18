4ea9326e0ca91b7bcec6042d0f3761e18f2c9edf
Mitch Guzman <mguzman9294@gmail.com>
2017-03-18 09:45:35 -0700

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Mitch Guzman on 3/18/17.
+//  Copyright © 2017 Mitch Guzman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
